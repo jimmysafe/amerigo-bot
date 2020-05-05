d7621ee6fd6d8f50e860378eb7e632bc2faf4e44
jimmysafe <ciaffardini.g@gmail.com>
2020-05-05 20:43:59 +0100

    structure server

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,6 +2,6 @@ const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
   app.use(
-    proxy("/srv", { target: "http://localhost:9000" })
+    proxy("/api", { target: "http://localhost:9000" })
   );
 };
