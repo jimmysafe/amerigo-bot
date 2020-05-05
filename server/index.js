28b3a14cc3f747742520f1fa7a4e4d7976100bff
jimmysafe <ciaffardini.g@gmail.com>
2020-05-05 20:48:38 +0100

    structure server

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const cors = require('cors')
 const fileUpload = require('express-fileupload')
 const fs = require('fs')
+const path = require('path')
 
 const app = express()
 const client = new Discord.Client();
@@ -17,11 +18,10 @@ const admin = require('./routes/admin')
 // Middlewares
 app.use(cors())
 app.use(fileUpload())
-app.use(express.static('uploads'))
+app.use(express.static(path.join(__dirname, 'uploads')))
 app.use(express.json())
 
 // Routes Definition
-app.get('/', (req, res) => res.send("This Root Works!"))
 app.get('/api', (req, res) => res.send("This API Works!"))
 app.use('/api/youtube', youtube)
 app.use('/api/admin', admin)
